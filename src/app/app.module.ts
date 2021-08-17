72f5ceb367cbbdbe1c5ea82d5addbc56b7444dee
Jain, Akshat <akshatjain76@deloitte.com>
2021-08-17 10:32:25 +0530

    app.module.ts changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,27 +8,18 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ModalComponent } from './components/modal/modal.component';
-import { FilterComponent } from './components/filter/filter.component';
 import { EmployeeComponent } from './components/employee/employee.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
-import { CardComponent } from './components/card/card.component';
 import { SearchComponent } from './components/search/search.component';
-import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
-import { CompanyDashboardComponent } from './pages/company-dashboard/company-dashboard.component';
-import { EmployeeDashboardComponent } from './pages/employee-dashboard/employee-dashboard.component';
 import { DashboardComponent } from './pages/employee/dashboard/dashboard.component';
 import { ProfileComponent } from './pages/company/profile/profile.component';
 import { PendingKycComponent } from './pages/admin/pending-kyc/pending-kyc.component';
 import { AcceptedKycComponent } from './pages/admin/accepted-kyc/accepted-kyc.component';
 import { RejectedKycComponent } from './pages/admin/rejected-kyc/rejected-kyc.component';
 import { HomeComponent } from './pages/home/home.component';
-import { SingleComponent } from './pages/company/register/single/single.component';
 import { RegisterComponent } from './pages/company/register/register.component';
 import { EmployeesComponent } from './pages/company/employees/employees.component';
-import { PersonalDetailsComponent } from './pages/employee/personal-details/personal-details.component';
-import { PersonalDocumentsComponent } from './pages/employee/personal-documents/personal-documents.component';
-import { ProfilePictureComponent } from './pages/employee/profile-picture/profile-picture.component';
 import { LivelinessComponent } from './pages/employee/liveliness/liveliness.component';
 import { ImageComponent } from './pages/employee/image/image.component';
 import { DocumentsComponent } from './pages/employee/documents/documents.component';
@@ -42,27 +33,18 @@ import { ReviewComponent } from './pages/employee/review/review.component';
     FooterComponent,
     SidebarComponent,
     ModalComponent,
-    FilterComponent,
     EmployeeComponent,
     LoginComponent,
     SignupComponent,
-    CardComponent,
     SearchComponent,
-    AdminDashboardComponent,
-    CompanyDashboardComponent,
-    EmployeeDashboardComponent,
     DashboardComponent,
     ProfileComponent,
     PendingKycComponent,
     AcceptedKycComponent,
     RejectedKycComponent,
     HomeComponent,
-    SingleComponent,
     RegisterComponent,
     EmployeesComponent,
-    PersonalDetailsComponent,
-    PersonalDocumentsComponent,
-    ProfilePictureComponent,
     LivelinessComponent,
     ImageComponent,
     DocumentsComponent,
