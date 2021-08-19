36921a73e2d1bc9b7b7e6c71be4e0f90f0e1d2c4
Riya Punjabi <rpunjabi@deloitte.com>
2021-08-19 15:01:58 +0530

    Basic Connectivity with backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CompanyService } from './services/company/company.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  response:any
+  companyService: CompanyService
+  constructor(companyService:CompanyService)
+  {
+    this.companyService=companyService;
+
+  }
+  ngOnInit()
+  {
+    this.response=this.companyService.getCompanies();
+
+  }
+
+
 }
--- src/app/services/company/company.service.ts
+++ src/app/services/company/company.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
 
-  constructor() { }
+  constructor(private httpClient :HttpClient) { }
+  getCompanies():
+  Observable<any>{
+    return this.httpClient.get('http://localhost:8085/company/employees/1');
+
+  }
+
 }
+
