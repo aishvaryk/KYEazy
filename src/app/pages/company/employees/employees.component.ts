f5e730f32fb494eaf79898633d338e5ae0c09e62
Aishvary Khandelwal <aikhandelwal@deloitte.com>
2021-08-23 08:34:44 +0530

    added filter, sort, search and pagination in employee list

--- src/app/pages/company/employees/employees.component.ts
+++ src/app/pages/company/employees/employees.component.ts
@@ -3,59 +3,84 @@ import { Component, OnInit } from '@angular/core';
 import { MediaChange, MediaObserver } from '@angular/flex-layout';
 
 import { filter, map } from 'rxjs/operators';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+
+export interface paginator {
+  length: number;
+  currentPageIndex: number;
+  currentPageSize: number;
+  pageSizeOptions: Array<number>;
+}
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.scss']
+  styleUrls: ['./employees.component.scss'],
 })
 export class EmployeesComponent implements OnInit {
   emailFormControl = new FormControl('');
-
-  verificationStatus:String;
   private observable: any;
   public isSmall: boolean;
-  public flexDirection: String;
-  public fxLayoutAlign:String;
-  public fxContainerLayoutAlign: String;
+  public paginator: paginator;
 
+  public filter: string;
+  public sortBy: string;
+  public search: string;
+
+  verificationStatus: String;
   constructor(public observer: MediaObserver) {
-    this.verificationStatus = "verified";
-    this.flexDirection = "row";
+    this.verificationStatus = 'verified';
     this.isSmall = false;
-    this.fxLayoutAlign = "space-between center";
-    this.fxContainerLayoutAlign = "center";
-
-   }
+    this.paginator = {
+      length: 100,
+      currentPageSize: 10,
+      pageSizeOptions: [5, 10, 25, 100],
+      currentPageIndex: 0,
+    };
+    this.filter = '';
+    this.sortBy = '';
+    this.search = '';
+  }
 
   ngOnInit(): void {
-    this.observable = this.observer.asObservable().pipe(
-      filter((changes: MediaChange[]) => changes.length > 0),
-      map((changes: MediaChange[]) => changes[0])
-    ).subscribe((change: MediaChange) => {
-      if (change.mqAlias === 'xs') {
-        this.isSmall = true;
-        this.flexDirection = "column";
-        this.fxLayoutAlign = "space-between start";
-
-        this.fxContainerLayoutAlign = "space-around center";
-      } else {
-        this.isSmall = false;
-        this.flexDirection = "row";
-
-        this.fxContainerLayoutAlign = "center";
-        this.fxLayoutAlign = "space-between center";
-      }
-    });
+    this.observable = this.observer
+      .asObservable()
+      .pipe(
+        filter((changes: MediaChange[]) => changes.length > 0),
+        map((changes: MediaChange[]) => changes[0])
+      )
+      .subscribe((change: MediaChange) => {
+        if (change.mqAlias === 'xs') {
+          this.isSmall = true;
+        } else {
+          this.isSmall = false;
+        }
+      });
   }
 
   numSequence(n: number): Array<number> {
     return Array(n);
   }
 
-  OnPageChange(event:any) {
-    console.log(event);
+  OnPageChange(event: any) {
+    this.paginator.currentPageIndex = event.pageIndex;
+    this.paginator.currentPageSize = event.pageSize;
+    console.log(this.paginator);
   }
 
+  OnSortSelect(event: any) {
+    console.log(event.value);
+    this.sortBy = event.value;
+  }
+
+  OnFilterSelect(event: any) {
+    console.log(event.value);
+    this.filter = event.value;
+  }
 }
