e1a8eab133bda102b2f92f4066c5097359d195d5
Aishvary Khandelwal <aikhandelwal@deloitte.com>
2021-09-01 18:13:22 +0530

    redisigned view profile page

--- src/app/pages/company/company-dashboard/company-dashboard.component.ts
+++ src/app/pages/company/company-dashboard/company-dashboard.component.ts
@@ -8,25 +8,26 @@ import { CompanyService } from 'src/app/services/company/company.service';
 import { pieChartData } from 'src/app/models/pieChartData.model';
 import { Breakpoint } from 'src/app/models/breakpoint.model';
 import { Store } from '@ngrx/store';
-import { Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 // import { ChartType, ChartOptions } from 'chart.js';
 // import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
 
 @Component({
   selector: 'app-company-dashboard',
   templateUrl: './company-dashboard.component.html',
-  styleUrls: ['./company-dashboard.component.scss']
+  styleUrls: ['./company-dashboard.component.scss'],
 })
 export class CompanyDashboardComponent implements OnInit {
   public companyService: CompanyService;
-  public employees:Employee[];
-  public company:Company;
-  public isSmall:any;
+  public employees: Employee[];
+  public company: Company;
+  public isSmall: any;
+  public zeroEmployees: any;
 
   breakpoint$: Observable<Breakpoint>;
   chartView: any = [400, 300];
   legendView: any = [300, 300];
-  pieChartData:any;
+  pieChartData: any;
 
   // options
   gradient: boolean = true;
@@ -37,71 +38,75 @@ export class CompanyDashboardComponent implements OnInit {
   isDoughnut: boolean = false;
 
   legendPosition: any = 'below';
-  colorScheme :any = {
-    domain: ['green', 'red', '#FC9A1D']
+  colorScheme: any = {
+    domain: ['slategrey', 'green', 'red', '#FC9A1D'],
   };
 
-
-  constructor(public store: Store<{breakpoint: Breakpoint}>, companyService:CompanyService) {
+  constructor(
+    public store: Store<{ breakpoint: Breakpoint }>,
+    companyService: CompanyService
+  ) {
     this.breakpoint$ = store.select('breakpoint');
     this.breakpoint$.subscribe((breakpoint) => {
-      if (breakpoint.isXs ) {
-        this.isSmall=true
+      if (breakpoint.isXs) {
+        this.isSmall = true;
 
-  this.chartView = [340, 300];
+        this.chartView = [340, 300];
       } else {
-        this.isSmall=false;
-        this.chartView = [400,300]
+        this.isSmall = false;
+        this.chartView = [400, 300];
       }
     });
 
-    pieChartData[0].value=343;
-      pieChartData[1].value=443;
-      pieChartData[2].value=535;
-    this.companyService=companyService;
-    this.employees=[{}] as Employee[];
-    this.company={} as Company;
+    this.companyService = companyService;
+    this.employees = [{}] as Employee[];
+    this.company = {} as Company;
     console.log(pieChartData);
     this.companyService.getCompanyDetails(1);
-    this.companyService.companySubject.subscribe((company)=>{
-      this.company=company;
-      pieChartData[0].value=this.company.numberOfAcceptedEmployees;
-      pieChartData[1].value=this.company.numberOfRejectedEmployees;
-      pieChartData[2].value=this.company.numberOfPendingEmployees;
-
-
-      Object.assign(this, {pieChartData} );
-    }
-    );
-
+    this.companyService.companySubject.subscribe((company) => {
+      this.company = company;
+      if (this.company.numberOfTotalEmployees === 0) {
+        this.zeroEmployees = true;
+      } else {
+        this.zeroEmployees = false;
+      }
+      console.log(pieChartData,"pehle");
+      console.log(this.company.numberOfTotalEmployees);
+      pieChartData[0].value =
+        this.company.numberOfTotalEmployees -
+        this.company.numberOfAcceptedEmployees -
+        this.company.numberOfRejectedEmployees -
+        this.company.numberOfPendingEmployees;
+      pieChartData[1].value = this.company.numberOfAcceptedEmployees;
+      pieChartData[2].value = this.company.numberOfRejectedEmployees;
+      pieChartData[3].value = this.company.numberOfPendingEmployees;
+      console.log(pieChartData);
+      Object.assign(this, { pieChartData });
+    });
   }
 
-  onSelect(data:any): void {
+  onSelect(data: any): void {
     console.log('Item clicked', JSON.parse(JSON.stringify(data)));
   }
 
-  onActivate(data:any): void {
+  onActivate(data: any): void {
     console.log('Activate', JSON.parse(JSON.stringify(data)));
   }
 
-  onDeactivate(data:any): void {
+  onDeactivate(data: any): void {
     console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
   ngOnInit(): void {
-    this.companyService.getEmployeesSortedByDate(1,2,1);
-    this.companyService.employeesSubject.subscribe((employees)=>{
-      this.employees=employees;
+    this.companyService.getEmployeesSortedByDate(1, 2, 1);
+    this.companyService.employeesSubject.subscribe((employees) => {
+      this.employees = employees;
       console.log(employees);
-    }
-    );
+    });
   }
   formatImage(img: any): any {
-
     if (img == null) {
       return null;
     }
     return 'data:image/jpeg;base64,' + img;
   }
-
-
 }
