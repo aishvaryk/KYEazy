e064c49a23265a96b052a94a706818fda28435fc
Riya Punjabi <rpunjabi@deloitte.com>
2021-09-05 21:51:09 +0530

    Order History

--- src/app/pages/company/subscription/subscription.component.ts
+++ src/app/pages/company/subscription/subscription.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Company } from 'src/app/models/company.model';
 import { CompanyService } from 'src/app/services/company/company.service';
 import { PaymentService } from 'src/app/services/payment/payment.service';
 
@@ -11,40 +10,62 @@ import { PaymentService } from 'src/app/services/payment/payment.service';
 export class SubscriptionComponent implements OnInit {
 
   rzp:any;
-  pack!:string
-  amount!:string
-  coinBalance!:number
-
+  pack!:string;
+  amount!:string;
+  coinBalance!:number;
+  coinsToAdd!:number;
+  planToActivate!:number;
+  companyId!:number;
+  orderHistory=[]
   constructor(private paymentService:PaymentService,private companyService:CompanyService) { }
 
   ngOnInit(): void {
     let k=localStorage.getItem("Id");
-    if(k!=null) this.companyService.getCompanyDetails(parseInt(k));
+    console.log(k)
+    if(k!=null)
+    {
+    this.companyId=parseInt(k);
+      console.log(this.companyId)
+    this.companyService.getCompanyDetails(this.companyId);
+    }
     this.companyService.companySubject.subscribe((company)=>{
+    //  this.companyId=company.companyId;
       this.coinBalance=company.coins;
+      this.options.prefill.name=company.name;
+      this.planToActivate=company.plan;
+      //this.options.prefill.contact=company.;
     })
+    this.paymentService.getOrderHistory(this.companyId)
+    this.paymentService.orderHistory.subscribe((p)=>{
+      console.log(p)
+      this.orderHistory=p;
 
+    })
+}
+  setCoins(event:any)
+  {
+    this.coinsToAdd=event.target.value
   }
+
   options = {
-    "key": "rzp_test_51mlvZBHt5Cbjq", // Enter the Key ID generated from the Dashboard
-    "amount": "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "key": "rzp_test_51mlvZBHt5Cbjq",
+    "amount": "",
     "currency": "INR",
     "name": "Kyeazzy",
     "description": "Subscription",
     "image": "https://example.com/your_logo",
-    "order_id": "", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+    "order_id": "",
     "handler":  (response:any)=>{
-        // alert(response.razorpay_payment_id);
-        // alert(response.razorpay_order_id);
-        // alert(response.razorpay_signature)
-        console.log(response)
-//        this.paymentService.paymentSuccess(response.razorpay_payment_id,response.razorpay_order_id,response.razorpay_signature)
+         alert(response.razorpay_payment_id);
+          console.log(response)
+          console.log(this.coinsToAdd)
+        this.paymentService.paymentSuccess(this.companyId,this.coinsToAdd,response.razorpay_payment_id,this.options.order_id)
 
       },
     "prefill": {
         "name": "Gaurav Kumar",
         "email": "gaurav.kumar@example.com",
-        "contact": "9999999999"
+        //"contact": "9999999999"
     },
     "notes": {
         "address": "Razorpay Corporate Office"
@@ -53,19 +74,19 @@ export class SubscriptionComponent implements OnInit {
         "color": "#3399cc"
     }
 };
-  pay(coins:any)
+  pay()
   {
-    let amount=200*coins;
+    console.log(this.planToActivate)
+    console.log(this.coinsToAdd)
+    let amount=this.planToActivate*this.coinsToAdd*100;
+
     this.options.amount=amount.toString();
-    this.options.prefill={
-      "name": "Riya Punjabi",
-      "email": "riyapunjabi16092000@gmail.com",
-      "contact": "9098679391"
-  };
+
     this.paymentService.getOrderId(this.options.amount);
     this.paymentService.orderSubject.subscribe((orderId)=>{this.options.order_id=orderId})
     this.rzp = new this.paymentService.nativeWindow.Razorpay(this.options);
     this.rzp.open();
   }
+
 
 }
