b5bf89e1908d402c6baf45f935c7646c9c37957a
Shekhar Gupta <shekgupta@deloitte.comshekgupta@deloitte.com>
2021-09-07 18:20:06 +0530

    learn more implemented

--- src/app/components/learn-more/learn-more.component.ts
+++ src/app/components/learn-more/learn-more.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
 
 @Component({
   selector: 'app-learn-more',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LearnMoreComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_BOTTOM_SHEET_DATA)
+    public data: {
+      companyType: string;
+    }
+  ) { }
 
   ngOnInit(): void {
   }
